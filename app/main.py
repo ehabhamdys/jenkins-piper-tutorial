e98508d45e07d169d764e4dc8403186f3656bc21
Ehab Hussein <ehab.hussein@sap.com>
2024-06-21 12:11:58 +0200

    Add initial project files and configuration

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+def create_app():
+    app = FastAPI()
+
+    @app.get("/")
+    def read_root():
+        return {"message": "Hello, FastAPI"}
+
+    @app.get("/items/{item_id}")
+    def read_item(item_id: int, query_param: str = None):
+        return {"item_id": item_id, "query_param": query_param}
+
+    return app
+
+def main():
+    print("Hello World")
+    uvicorn.run("app.main:create_app", host="localhost", port=8080, reload=True, workers=2)
+
+if __name__ == "__main__":
+    main()
